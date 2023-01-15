da4fb27aa9ba9bd8754bc740c8e294eb1aa52c3e
Victor Motta <victor.mottag@gmail.com>
2023-01-15 03:50:36 -0300

    Fazendo bonus deletar, e update

--- src/app.js
+++ src/app.js
@@ -78,29 +78,10 @@ app.get("/messages", async (req, res) => {
     if (limit <= 0) {
         return res.sendStatus(422);
     }
-
     if (!user) {
         return res.sendStatus(422);
     }
-
     try {
-        console.log("entrou");
-        // let messagesFiltered = await messages
-        //     .filter((message) => {
-        //         if (message.to === "Todos") {
-        //             return message;
-        //         }
-        //     })
-        //     .filter((message) => {
-        //         if (message.to === user) {
-        //             return message;
-        //         }
-        //     })
-        //     .filter((message) => {
-        //         if (message.from === user) {
-        //             return message;
-        //         }
-        //     });
         // const messageSend = messages.map((message) => {
         //     return { to: message.to, text: message.text, type: message.type, from: message.from };
         // });
@@ -156,6 +137,86 @@ app.post("/messages", async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(401).send("Erro ao enviar!");
+    }
+});
+
+app.put("/messages/:id", async (req, res) => {
+    const { id } = req.params;
+    const user = req.headers.user;
+    const { to, text, type } = req.body;
+
+    let message;
+
+    const messagePutSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid("message").valid("private_message").required(),
+        from: joi.string().required(),
+    });
+
+    const validation = messagePutSchema.validate(
+        { to, text, type, from: user },
+        { abortEarly: false }
+    );
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    const verifyUser = await db.collection("participants").findOne({ name: user });
+
+    if (!verifyUser) {
+        return res.sendStatus(422);
+    }
+
+    try {
+        message = await db.collection("messages").findOne({ _id: ObjectId(id) });
+
+        if (message.from != user) {
+            return res.sendStatus(401);
+        }
+
+        await db
+            .collection("messages")
+            .updateOne({ _id: ObjectId(id) }, { $set: { to, text, type } });
+
+        message = await db.collection("messages").findOne({ _id: ObjectId(id) });
+
+        res.send(message);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(404);
+    }
+});
+
+app.delete("/messages/:id", async (req, res) => {
+    const { id } = req.params;
+    const user = req.headers.user;
+
+    if (!user) {
+        return res.sendStatus(422);
+    }
+
+    const verifyUser = await db.collection("participants").findOne({ name: user });
+
+    if (!verifyUser) {
+        return res.sendStatus(422);
+    }
+
+    try {
+        const message = await db.collection("messages").findOne({ _id: ObjectId(id) });
+
+        if (message.from != user) {
+            return res.sendStatus(401);
+        }
+
+        await db.collection("messages").deleteOne({ _id: ObjectId(id) });
+
+        return res.send("Ok");
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(404);
     }
 });
 
